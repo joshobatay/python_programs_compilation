c86b31a06ad07945c7434ff3f7410c6b9abe4cea
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 18:01:10 +0800

    added try-except for error handling

--- input_10_num_print_sum_of_all_num.py
+++ input_10_num_print_sum_of_all_num.py
@@ -1,19 +1,25 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
-# ask user to input 10 numbers
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
-num4 = int(input("Enter fourth number: "))
-num5 = int(input("Enter fifth number: "))
-num6 = int(input("Enter sixth number: "))
-num7 = int(input("Enter seventh number: "))
-num8 = int(input("Enter eighth number: "))
-num9 = int(input("Enter ninth number: "))
-num10 = int(input("Enter tenth number: "))
+# try-except for error handling
+try:
+    
+    # ask user to input 10 numbers
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    num3 = int(input("Enter third number: "))
+    num4 = int(input("Enter fourth number: "))
+    num5 = int(input("Enter fifth number: "))
+    num6 = int(input("Enter sixth number: "))
+    num7 = int(input("Enter seventh number: "))
+    num8 = int(input("Enter eighth number: "))
+    num9 = int(input("Enter ninth number: "))
+    num10 = int(input("Enter tenth number: "))
 
-# process numbers
-sum_of_all = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10
+    # process numbers
+    sum_of_all = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10
 
-# print result
-print(f"The sum of all numbers is {sum_of_all}")
+    # print result
+    print(f"The sum of all numbers is {sum_of_all}")
+
+except ValueError:
+    print("Please enter a valid number")
