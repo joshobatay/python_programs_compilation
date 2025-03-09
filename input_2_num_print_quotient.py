4315c4793da441362b2cd58d15e54c8df0b59c98
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 17:29:34 +0800

    added input

--- input_2_num_print_quotient.py
+++ input_2_num_print_quotient.py
@@ -1,7 +1,10 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
 # ask user to input 2 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 # process quotient
+
 
 # print result
