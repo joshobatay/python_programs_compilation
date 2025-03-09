b13679c7defedd6422095f5eb6c744a20afaa2b6
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 16:47:09 +0800

    added input

--- input_2_num_print_equal_when_same.py
+++ input_2_num_print_equal_when_same.py
@@ -1,6 +1,8 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
 # ask user for 2 numbers
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
 
 # print equal if same
 
