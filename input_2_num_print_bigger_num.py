4216126e97efd5f5a5d775e00db44645e4f94f4a
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 16:39:58 +0800

    added try-except for error handling

--- input_2_num_print_bigger_num.py
+++ input_2_num_print_bigger_num.py
@@ -1,11 +1,19 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-num1 = int(input('Enter 1st number: '))
-num2 = int(input('Enter 2nd number: '))
 
-# 2. print bigger number
-if num1 > num2:
-    print(f'Bigger number is: {num1}')
-elif num2 > num1:
-    print(f'Bigger number is: {num2}')
-else:
-    print('Both numbers are equal')
+# try-except for error handling
+try:
+    
+    # 1. get 2 numbers from user
+    num1 = int(input('Enter 1st number: '))
+    num2 = int(input('Enter 2nd number: '))
+
+    # 2. print bigger number
+    if num1 > num2:
+        print(f'Bigger number is: {num1}')
+    elif num2 > num1:
+        print(f'Bigger number is: {num2}')
+    else:
+        print('Both numbers are equal!')
+
+except ValueError:
+    print('Please enter valid numbers!')
