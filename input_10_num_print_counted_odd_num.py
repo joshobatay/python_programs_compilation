adce452c18bded47470dd7e2777ddcadbbe72178
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 18:19:41 +0800

    added try-except for error handling

--- input_10_num_print_counted_odd_num.py
+++ input_10_num_print_counted_odd_num.py
@@ -1,40 +1,46 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
-# ask user to input 10 numbers
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
-num4 = int(input("Enter fourth number: "))
-num5 = int(input("Enter fifth number: "))
-num6 = int(input("Enter sixth number: "))
-num7 = int(input("Enter seventh number: "))
-num8 = int(input("Enter eighth number: "))
-num9 = int(input("Enter ninth number: "))
-num10 = int(input("Enter tenth number: "))
+# try-except for error handling
+try:
 
-# process numbers
-odd_count = 0
+    # ask user to input 10 numbers
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    num3 = int(input("Enter third number: "))
+    num4 = int(input("Enter fourth number: "))
+    num5 = int(input("Enter fifth number: "))
+    num6 = int(input("Enter sixth number: "))
+    num7 = int(input("Enter seventh number: "))
+    num8 = int(input("Enter eighth number: "))
+    num9 = int(input("Enter ninth number: "))
+    num10 = int(input("Enter tenth number: "))
 
-if num1 % 2 != 0:
-    odd_count += 1
-if num2 % 2 != 0:
-    odd_count += 1
-if num3 % 2 != 0:
-    odd_count += 1
-if num4 % 2 != 0:
-    odd_count += 1
-if num5 % 2 != 0:
-    odd_count += 1
-if num6 % 2 != 0:
-    odd_count += 1
-if num7 % 2 != 0:
-    odd_count += 1
-if num8 % 2 != 0:
-    odd_count += 1
-if num9 % 2 != 0:
-    odd_count += 1
-if num10 % 2 != 0:
-    odd_count += 1
-    
-# print result
-print(f"The number of odd numbers is {odd_count}")
+    # process numbers
+    odd_count = 0
+
+    if num1 % 2 != 0:
+        odd_count += 1
+    if num2 % 2 != 0:
+        odd_count += 1
+    if num3 % 2 != 0:
+        odd_count += 1
+    if num4 % 2 != 0:
+        odd_count += 1
+    if num5 % 2 != 0:
+        odd_count += 1
+    if num6 % 2 != 0:
+        odd_count += 1
+    if num7 % 2 != 0:
+        odd_count += 1
+    if num8 % 2 != 0:
+        odd_count += 1
+    if num9 % 2 != 0:
+        odd_count += 1
+    if num10 % 2 != 0:
+        odd_count += 1
+        
+    # print result
+    print(f"The number of odd numbers is {odd_count}")
+
+except ValueError: 
+    print("Please enter a valid number")
