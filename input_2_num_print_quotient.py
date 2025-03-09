6da43fd28365b04adc6e2b20b873091ac7f5188b
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 17:39:36 +0800

    added try-except for error handling

--- input_2_num_print_quotient.py
+++ input_2_num_print_quotient.py
@@ -1,16 +1,21 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
-# ask user to input 2 numbers
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# try-except for error handling
+try: 
+    
+    # ask user to input 2 numbers
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-# process quotient
-if num2 == 0:
-    print("Cannot divide by zero")
-else:
-    quotient = num1 / num2
-    # print result
-    print(f"The quotient of {num1} and {num2} is {quotient}")
+    # process quotient
+    if num2 == 0:
+        print("Cannot divide by zero")
+    else:
+        quotient = num1 / num2
+        # print result
+        print(f"The quotient of {num1} and {num2} is {quotient}")
 
+except ValueError:
+    print("Please enter a valid number")
 
     
