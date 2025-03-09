d62b3fbf917b0592181e8a06e244985162fb1e45
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 17:12:25 +0800

    added try-except for error handling

--- input_2_num_print_sum.py
+++ input_2_num_print_sum.py
@@ -1,11 +1,15 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
-# ask 2 numbers from user
-num1 = int(input('Enter 1st number: '))
-num2 = int(input('Enter 2nd number: '))
+# try-except for error handling
+try: 
+    # ask 2 numbers from user
+    num1 = int(input('Enter 1st number: '))
+    num2 = int(input('Enter 2nd number: '))
 
-# process sum
-sum = num1 + num2
+    # process sum
+    sum = num1 + num2
 
-# print sum
-print(f'Sum of {num1} and {num2} is: {sum}')
+    # print sum
+    print(f'Sum of {num1} and {num2} is: {sum}')
+except ValueError:
+    print('Please enter valid numbers!')
