6a5997e417b3f4bd903f34d491f79347bc1c2132
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 17:42:28 +0800

    added input

--- input_2_num_print_power.py
+++ input_2_num_print_power.py
@@ -1,6 +1,8 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
 # ask user to input 2 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 # process numbers
 
