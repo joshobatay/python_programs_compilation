6f46aaca10a1d8a015c63d427c80c7c060266378
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 18:07:07 +0800

    added input

--- input_10_num_print_counted_odd_num.py
+++ input_10_num_print_counted_odd_num.py
@@ -1,6 +1,16 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 # ask user to input 10 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+num5 = int(input("Enter fifth number: "))
+num6 = int(input("Enter sixth number: "))
+num7 = int(input("Enter seventh number: "))
+num8 = int(input("Enter eighth number: "))
+num9 = int(input("Enter ninth number: "))
+num10 = int(input("Enter tenth number: "))
 
 # process numbers
 
