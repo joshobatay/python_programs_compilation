4265ad34085482484ec3eeb0b89afc5f26603afd
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 17:46:53 +0800

    added try-except for error handling

--- input_2_num_print_power.py
+++ input_2_num_print_power.py
@@ -1,11 +1,17 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
-# ask user to input 2 numbers
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# try-except for error handling
+try:
+    
+    # ask user to input 2 numbers
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-# process numbers
-power = num1 ** num2
+    # process numbers
+    power = num1 ** num2
 
-# print result
-print(f"{num1} raised to the power of {num2} is {power}")
+    # print result
+    print(f"{num1} raised to the power of {num2} is {power}")
+    
+except ValueError:
+    print("Please enter a valid number")
