0f86f99a236113e1d95b92f672862c58aa34ec34
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 19:13:13 +0800

    added process, output, and comments for clear understanding

--- print_num_from_0_to_100_except_ending_0.py
+++ print_num_from_0_to_100_except_ending_0.py
@@ -1,5 +1,12 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 # process
+i = 0
+while i <= 100:
+    # check if i is not divisible by 10 and 5, if not, it prints
+    if i % 10 != 0 and i % 5 != 0:
+        # output
+        print(i) 
+    # after i prints, i will increment by 1
+    i += 1
 
-# output
