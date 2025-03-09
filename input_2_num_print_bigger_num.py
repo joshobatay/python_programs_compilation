028a7f5017013f4bf61593a67ffe0c36bb6680d7
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 16:31:24 +0800

    code added

--- input_2_num_print_bigger_num.py
+++ input_2_num_print_bigger_num.py
@@ -1,6 +1,11 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-
-# 1. input 2 numbers
-
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
 
 # 2. print bigger number
+if num1 > num2:
+    print(f'Bigger number is: {num1}')
+elif num2 > num1:
+    print(f'Bigger number is: {num2}')
+else:
+    print('Both numbers are equal')
