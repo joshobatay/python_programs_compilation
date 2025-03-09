49b314fe177f4c89aea1a568a6188d7ce9713667
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 18:51:34 +0800

    added process and output

--- print_even_num_from_0_to_100.py
+++ print_even_num_from_0_to_100.py
@@ -1,5 +1,7 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
 # process
-
-# output
+for i in range(100):
+    if i % 2 == 0:
+        # output
+        print(i)
