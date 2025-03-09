6907ba045ac4c5135e270512fa82cf44088ffb5d
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 17:06:28 +0800

    added input

--- input_2_num_print_sum.py
+++ input_2_num_print_sum.py
@@ -1,5 +1,10 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 # ask 2 numbers from user
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+
+# process sum
+
 
 # print sum
