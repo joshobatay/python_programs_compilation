38ea9dd63303c7b712996f517107497abdcb2895
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 17:54:34 +0800

    added input

--- input_10_num_print_sum_of_all_num.py
+++ input_10_num_print_sum_of_all_num.py
@@ -1,6 +1,16 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 # ask user to input 10 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+num5 = int(input("Enter fifth number: "))
+num6 = int(input("Enter sixth number: "))
+num7 = int(input("Enter seventh number: "))
+num8 = int(input("Enter eighth number: "))
+num9 = int(input("Enter ninth number: "))
+num10 = int(input("Enter tenth number: "))
 
 # process numbers
 
