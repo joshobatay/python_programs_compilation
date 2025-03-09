fb2c3af50f00cd56a0d416bebee4b8d618c7e95c
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 16:51:30 +0800

    added try-except for error handling

--- input_2_num_print_equal_when_same.py
+++ input_2_num_print_equal_when_same.py
@@ -1,13 +1,17 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
-# ask user for 2 numbers
-num1 = int(input('Enter 1st number: '))
-num2 = int(input('Enter 2nd number: '))
-
-# print equal if same
-if num1 == num2:
-    print('Equal!')
-else:
-    print('Not equal!')
+# try-except for error handling
+try:
     
+    # ask user for 2 numbers
+    num1 = int(input('Enter 1st number: '))
+    num2 = int(input('Enter 2nd number: '))
 
+    # print equal if same
+    if num1 == num2:
+        print('Equal!')
+    else:
+        print('Not equal!')
+
+except ValueError:
+    print('Please enter valid numbers!')
