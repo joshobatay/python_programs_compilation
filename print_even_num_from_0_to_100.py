3cdf50d2a5f0e6544f5024ad1db95c48e1c14ab6
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 18:52:42 +0800

    changed range(100) to range(0, 101)

--- print_even_num_from_0_to_100.py
+++ print_even_num_from_0_to_100.py
@@ -1,7 +1,7 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
 # process
-for i in range(100):
+for i in range(0, 101):
     if i % 2 == 0:
         # output
         print(i)
