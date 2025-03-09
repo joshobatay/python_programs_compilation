5c2e0f9058998f37edbd839e389acdd913af4234
Josh Obatay <joshobatay2005@gmail.com>
2025-03-09 17:28:28 +0800

    pseudocode

---
+++ input_2_num_print_quotient.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+# ask user to input 2 numbers
+
+# process quotient
+
+# print result
