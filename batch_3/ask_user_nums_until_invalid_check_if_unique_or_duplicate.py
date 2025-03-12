168f75d146e1d2df45f96d313cef1337dc444299
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 19:24:56 +0800

    added an empty list for storing empty numbers, input, process, output, and try-except

--- batch_3/ask_user_nums_until_invalid_check_if_unique_or_duplicate.py
+++ batch_3/ask_user_nums_until_invalid_check_if_unique_or_duplicate.py
@@ -1,7 +1,26 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate
 
+# initialize an empty list for storing empty numbers
+entered_numbers = []
+
 # ask user infinite times until input is invalid
+while True:
+    # try-except for error handling
+    try: 
+        num = int(input("Enter a number: "))
+        
+        # process
+        if num in entered_numbers:
+            print("Duplicate!") # output
+        else:
+            print("Unique!")
+            entered_numbers.append(num) # output
+            
+    except ValueError:
+        print("Invalid input, please enter a number!")
+        break # the loop will stop if user input is invalid
+        
+    
 
-# process
 
-# output
+
