19796366da78de8f83be53fe2d94c8782cb881ea
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:38:41 +0800

    added input, process, and output

--- batch_2/input_2_numbers_print_not_equal_if_not_same.py
+++ batch_2/input_2_numbers_print_not_equal_if_not_same.py
@@ -1,7 +1,13 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 # input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 # process
+if num1 != num2:
+    print("Not equal!") # output
+else:
+    print('Equal!') # output
 
-# output
+
