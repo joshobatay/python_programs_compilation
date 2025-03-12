f14edbfc05609dd520f629c5f07166a2c19ffa90
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 18:33:27 +0800

    added input

--- batch_3/ask_user_10_nums_display_first_entry_only.py
+++ batch_3/ask_user_10_nums_display_first_entry_only.py
@@ -1,6 +1,8 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 # ask user 10 numbers
+for i in range(10):
+    num = int(input(f"Enter a number {i + 1}: "))
 
 # process
 
