728b6b8601258427226b34de2d1dcc99e57da4f0
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:55:16 +0800

    added input

--- batch_2/input_2_numbers_print_remainder.py
+++ batch_2/input_2_numbers_print_remainder.py
@@ -1,6 +1,8 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 
 # input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 # process
 
