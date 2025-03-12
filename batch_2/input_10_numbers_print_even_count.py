83f1a16742950998db358ff4ea1353bff6d15ca7
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 23:13:14 +0800

    changed {i+1} to {i + 1}

--- batch_2/input_10_numbers_print_even_count.py
+++ batch_2/input_10_numbers_print_even_count.py
@@ -7,7 +7,7 @@ try:
 
     # input
     for i in range(10): # ask user 10 times
-        num = int(input(f"Enter number {i+1}: "))
+        num = int(input(f"Enter number {i + 1}: "))
         if num % 2 == 0: # checks if number is divisible by 2, if yes, even_count adds by 1
             even_count+=1
             
