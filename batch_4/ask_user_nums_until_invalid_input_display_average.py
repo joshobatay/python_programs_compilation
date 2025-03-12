7862b1d1949c44a2502de7bebcd914ff59ad7ab5
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 22:58:13 +0800

    imported a built-in module of python and added an empty set

--- batch_4/ask_user_nums_until_invalid_input_display_average.py
+++ batch_4/ask_user_nums_until_invalid_input_display_average.py
@@ -1,5 +1,10 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
+import statistics # built in module of python for getting average
+
+# initialize an empty set for storing entered numbers
+entered_number = []
+
 # ask user infinite times
 
 # process
