49bfac1a9e33bdcdaad630f8426921bd5201572a
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:40:17 +0800

    added try-except for error handling

--- batch_2/input_2_numbers_print_not_equal_if_not_same.py
+++ batch_2/input_2_numbers_print_not_equal_if_not_same.py
@@ -1,13 +1,17 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
-# input
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# try-except for error handling
+try: 
 
-# process
-if num1 != num2:
-    print("Not equal!") # output
-else:
-    print('Equal!') # output
+    # input
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
+    # process
+    if num1 != num2:
+        print("Not equal!") # output
+    else:
+        print('Equal!') # output
 
+except ValueError:
+    print("Invalid input")
