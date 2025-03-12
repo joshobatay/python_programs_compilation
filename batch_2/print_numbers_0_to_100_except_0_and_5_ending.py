6fa36c35f97530339522149b7da7b40949591572
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 17:36:41 +0800

    final

--- batch_2/print_numbers_0_to_100_except_0_and_5_ending.py
+++ batch_2/print_numbers_0_to_100_except_0_and_5_ending.py
@@ -1,5 +1,6 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
 # process
-
-# output
+for i in range(100): # loops from 0 to 100
+    if i % 2 != 0 and i % 5 != 0:
+        print(i, end=" ") # outputs the numbers
