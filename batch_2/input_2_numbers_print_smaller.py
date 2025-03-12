f9fd18cb37549eada6e6b5500831b5ca27a1f983
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:32:23 +0800

    added input

--- batch_2/input_2_numbers_print_smaller.py
+++ batch_2/input_2_numbers_print_smaller.py
@@ -1,6 +1,8 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 # process
 
