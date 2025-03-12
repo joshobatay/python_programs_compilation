e5004841eab0577051f97eab9e3c8ba9a498128a
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 17:16:26 +0800

    added output

--- batch_2/input_10_numbers_print_even_count.py
+++ batch_2/input_10_numbers_print_even_count.py
@@ -6,7 +6,8 @@ even_count = 0
 # input
 for i in range(10): # ask user 10 times
     num = int(input(f"Enter number {i+1}: "))
-    if num % 2 == 0: # process
+    if num % 2 == 0: # checks if number is divisible by 2, if yes, even_count adds by 1
         even_count+=1
         
 # output
+print(f"There are {even_count} even numbers")
