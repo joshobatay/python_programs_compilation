43485c10cd5bc901b7027d9a364dfbbcd2537b5a
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:35:29 +0800

    added try-except for error handling

--- batch_2/input_2_numbers_print_smaller.py
+++ batch_2/input_2_numbers_print_smaller.py
@@ -1,16 +1,21 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
-# input
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# try-except for error handling
+try:
 
-# process
-if num1 < num2:
-    print(f"{num1} is smaller than {num2}") # output
-    
-elif num2 < num1:
-    print(f"{num2} is smaller than {num1}") # output
-else:
-    
-    print("Both numbers are equal") # output
+    # input
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
+    # process
+    if num1 < num2:
+        print(f"{num1} is smaller than {num2}") # output
+        
+    elif num2 < num1:
+        print(f"{num2} is smaller than {num1}") # output
+    else:
+        
+        print("Both numbers are equal") # output
+
+except ValueError:
+    print('Invalid input!')
