243097d87f995ccb7cd2661716c34996eb438dd3
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 17:24:20 +0800

    final

--- batch_2/print_odd_numbers_0_to_100_while_loop.py
+++ batch_2/print_odd_numbers_0_to_100_while_loop.py
@@ -1,5 +1,12 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
-# process
+# initialize a value
+i = 0
 
-# output
+# process
+while i <= 100:
+    if i % 2 != 0:
+        print(i) # output
+    i += 1 # adds 1 to i after the loop
+    
+
