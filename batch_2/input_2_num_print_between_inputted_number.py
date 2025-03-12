b752539f13a3724df6511a617d7c8165179ee531
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 17:45:55 +0800

    final and fully working

--- batch_2/input_2_num_print_between_inputted_number.py
+++ batch_2/input_2_num_print_between_inputted_number.py
@@ -1,7 +1,13 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # input
+num1 = int(input("Enter first number: "))
+num2 = int(input('Enter second number: '))
 
 # process
+if num2 < num1:
+    num1, num2 = num2, num1
 
 # output
+for i in range(num1 + 1, num2):
+    print(i, end=" ") 
