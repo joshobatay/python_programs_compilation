5ac14f0c64fddf48c3cb364d9b1d488c4f625a21
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 17:04:33 +0800

    initialized an empty list and added input

--- batch_2/input_10_numbers_print_first_minus_rest.py
+++ batch_2/input_10_numbers_print_first_minus_rest.py
@@ -1,6 +1,12 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
+# initialize an empty list
+numbers = []
+
 # input
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    numbers.append(num) # adds the number to the list
 
 # process
 
