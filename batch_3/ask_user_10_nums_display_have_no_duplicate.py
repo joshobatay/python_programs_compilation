a49698e721b95cba8dca307e33d4662818b43823
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 18:03:01 +0800

    added pseudocode and input

--- batch_3/ask_user_10_nums_display_have_no_duplicate.py
+++ batch_3/ask_user_10_nums_display_have_no_duplicate.py
@@ -1,7 +1,10 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # ask user 10 numbers
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
 
 # process
 
-# print numbers dont have duplicate
+# print numbers that dont have duplicate
+
