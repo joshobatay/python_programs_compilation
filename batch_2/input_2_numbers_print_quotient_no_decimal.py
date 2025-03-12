2eabe6263dde1931ca4c2b43acd83db3d211db38
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:49:05 +0800

    added input

--- batch_2/input_2_numbers_print_quotient_no_decimal.py
+++ batch_2/input_2_numbers_print_quotient_no_decimal.py
@@ -1,6 +1,8 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
 # input
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter a second number: "))
 
 # process
 
