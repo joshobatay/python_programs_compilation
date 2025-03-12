092c7fa037f70d17fa84e740d57af1ab35debeda
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 20:50:12 +0800

    added empty list for storing number and while true loop

--- batch_3/ask_user_nums_until_invalid_display_lowest_number.py
+++ batch_3/ask_user_nums_until_invalid_display_lowest_number.py
@@ -1,1 +1,13 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+# Initialize an empty list to store numbers
+entered_numbers = []
+
+while True:
+    
+        num = int(input("Enter a number: "))  # Ask for a number
+        entered_numbers.append(num)  # Stores the number
+        print(f"Lowest number so far: {min(entered_numbers)}")  # Show the lowest number
+    
+
+
