bd8a45d9b37f5874f724ec143107831482764023
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 21:32:35 +0800

    added more pseudocode and added input

--- batch_4/ask_user_10_num_display_number_that_have_duplicates.py
+++ batch_4/ask_user_10_num_display_number_that_have_duplicates.py
@@ -1,7 +1,16 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
+# initialize an empty list for storing entered number
+entered_numbers = []
+
 # ask user 10 numbers
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    entered_numbers.append(num)
+
+# initialize an empty list for counting duplicate numbers
 
 # process
 
 # display numbers that have duplicate
+
