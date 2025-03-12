30a75f822d81c255f63a04032307eddba12c42f8
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 22:55:10 +0800

    pseudocode

--- batch_4/ask_user_nums_until_invalid_input_display_average.py
+++ batch_4/ask_user_nums_until_invalid_input_display_average.py
@@ -1,1 +1,7 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
+# ask user infinite times
+
+# process
+
+# display average
