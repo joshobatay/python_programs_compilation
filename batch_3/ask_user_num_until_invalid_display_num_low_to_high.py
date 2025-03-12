4e3d0d2d563daa6b04e6acd160d1e468af7cb7d6
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 21:05:49 +0800

    added while true, input and output

--- batch_3/ask_user_num_until_invalid_display_num_low_to_high.py
+++ batch_3/ask_user_num_until_invalid_display_num_low_to_high.py
@@ -1,7 +1,16 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 
+# initialize an empty list for the numbers to be stored
+entered_numbers = []
+
 # ask user for number infinite times
+while True:
+    # process
+    
+        num = int(input("Enter a number: "))
+        entered_numbers.append(num)
+        entered_numbers.sort() # sorts the numbers 
+        print(f"Entered numbers (lowest to highest): {entered_numbers}") # outputs the numbers
 
-# process
 
-# output number from lowest to highest or display invalid input
+
