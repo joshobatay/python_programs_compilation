8a4a8060885cb61232dc94a996b349f34fd721cc
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:59:34 +0800

    added try-except for error handling

--- batch_2/input_2_numbers_print_remainder.py
+++ batch_2/input_2_numbers_print_remainder.py
@@ -1,11 +1,17 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 
-# input
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# try-except for error handling
+try:
+    
+    # input
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-# process
-remainder = num1 % num2 
+    # process
+    remainder = num1 % num2 
 
-# output
-print(f"The remainder of {num1} and {num2} is {remainder}")
+    # output
+    print(f"The remainder of {num1} and {num2} is {remainder}")
+
+except ValueError:
+    print("Invalid input!")
