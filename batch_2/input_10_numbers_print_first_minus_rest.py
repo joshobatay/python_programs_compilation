cf9009f2407f51d56d07ea0aee895e2f16585c3b
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 17:09:35 +0800

    added try-except for error handling

--- batch_2/input_10_numbers_print_first_minus_rest.py
+++ batch_2/input_10_numbers_print_first_minus_rest.py
@@ -1,15 +1,21 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
-# initialize an empty list
-numbers = []
+# try-except for error handling
+try:
 
-# input
-for i in range(10):
-    num = int(input(f"Enter number {i+1}: "))
-    numbers.append(num) # adds the number to the list
+    # initialize an empty list
+    numbers = []
 
-# process
-result = numbers[0] - sum(numbers[1:])
+    # input
+    for i in range(10):
+        num = int(input(f"Enter number {i+1}: "))
+        numbers.append(num) # adds the number to the list
 
-# output
-print(f"The result of the first number minus all the remaining numbers is: {result}")
+    # process
+    result = numbers[0] - sum(numbers[1:])
+
+    # output
+    print(f"The result of the first number minus all the remaining numbers is: {result}")
+
+except ValueError:
+    print("Invalid input!")
