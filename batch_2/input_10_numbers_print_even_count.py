f584bf8fca90843a1a146fc1d1a93f22bf2f961e
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 17:18:00 +0800

    added try-except for error handling

--- batch_2/input_10_numbers_print_even_count.py
+++ batch_2/input_10_numbers_print_even_count.py
@@ -1,13 +1,18 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
-# initialize a value
-even_count = 0
+# try-except for error handling
+try: 
+    # initialize a value
+    even_count = 0
 
-# input
-for i in range(10): # ask user 10 times
-    num = int(input(f"Enter number {i+1}: "))
-    if num % 2 == 0: # checks if number is divisible by 2, if yes, even_count adds by 1
-        even_count+=1
-        
-# output
-print(f"There are {even_count} even numbers")
+    # input
+    for i in range(10): # ask user 10 times
+        num = int(input(f"Enter number {i+1}: "))
+        if num % 2 == 0: # checks if number is divisible by 2, if yes, even_count adds by 1
+            even_count+=1
+            
+    # output
+    print(f"There are {even_count} even numbers")
+
+except ValueError:
+    print("Invalid input!")
