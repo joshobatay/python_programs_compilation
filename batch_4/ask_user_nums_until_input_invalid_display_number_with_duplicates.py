61835bb3ab8cbb2b1cdbfaa10f341be20229d2b1
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 22:21:26 +0800

    added while true input, process, and output

--- batch_4/ask_user_nums_until_input_invalid_display_number_with_duplicates.py
+++ batch_4/ask_user_nums_until_input_invalid_display_number_with_duplicates.py
@@ -1,11 +1,25 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate
 
 # initialize an empty list for storing entered number
+entered_numbers = []
 
-# ask user 10 numbers
+# ask user infinite times
+while True:
 
-# initialize an empty list for counting duplicate numbers
+        num = int(input('Enter number: '))
+        entered_numbers.append(num)
+    
+        # process, finds the number with the most duplicates
+        unique_numbers = set(entered_numbers) # a set that find unique numbers
+        most_frequent = max(unique_numbers, key=entered_numbers.count) # finds the most common number
+        count = entered_numbers.count(most_frequent) # counts the number of times a number appears
+        
+        # outputs the number with most duplicates
+        print(f"number {most_frequent}, appeared {count} times")
+        
+    
+  
 
-# process
 
-# display numbers that have duplicate
+
+
