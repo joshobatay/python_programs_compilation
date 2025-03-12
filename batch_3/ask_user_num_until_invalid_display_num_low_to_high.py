0416f3deb4fdcaef30ff18d4a47f7c1dcb27c00f
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 21:16:09 +0800

    added try-except and break

--- batch_3/ask_user_num_until_invalid_display_num_low_to_high.py
+++ batch_3/ask_user_num_until_invalid_display_num_low_to_high.py
@@ -5,12 +5,16 @@ entered_numbers = []
 
 # ask user for number infinite times
 while True:
-    # process
-    
+    try:
         num = int(input("Enter a number: "))
-        entered_numbers.append(num)
+        entered_numbers.append(num) # stores the numbers to the list
         entered_numbers.sort() # sorts the numbers 
         print(f"Entered numbers (lowest to highest): {entered_numbers}") # outputs the numbers
+        
+    except ValueError:
+        print("Invalid input, program will stop")
+        break # stops the program from an infinite loop
+    
 
 
 
