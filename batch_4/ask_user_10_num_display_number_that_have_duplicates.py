1feb10f18f1ad6e4aab8e0665d3e8ee8b0708f15
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 21:39:41 +0800

    added try-except for error handling

--- batch_4/ask_user_10_num_display_number_that_have_duplicates.py
+++ batch_4/ask_user_10_num_display_number_that_have_duplicates.py
@@ -3,19 +3,23 @@
 # initialize an empty list for storing entered number
 entered_numbers = []
 
-# ask user 10 numbers
-for i in range(10):
-    num = int(input(f"Enter number {i + 1}: "))
-    entered_numbers.append(num)
+try: 
+    # ask user 10 numbers
+    for i in range(10):
+        num = int(input(f"Enter number {i + 1}: "))
+        entered_numbers.append(num)
 
-# initialize an empty list for counting duplicate numbers
-duplicate_numbers = []
+    # initialize an empty list for counting duplicate numbers
+    duplicate_numbers = []
 
-# process
-for num in entered_numbers:
-    if entered_numbers.count(num) > 1 and num not in duplicate_numbers:
-        duplicate_numbers.append(num)
+    # process
+    for num in entered_numbers:
+        if entered_numbers.count(num) > 1 and num not in duplicate_numbers:
+            duplicate_numbers.append(num)
 
-# display numbers that have duplicate
-print(f"The numbers that have duplicates are {duplicate_numbers}")
+    # display numbers that have duplicate
+    print(f"The numbers that have duplicates are {duplicate_numbers}")
+
+except ValueError:
+    print("Invalid input, enter a valid number!")
 
