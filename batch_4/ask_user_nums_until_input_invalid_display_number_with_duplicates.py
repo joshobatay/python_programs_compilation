66abde974f148fd40d85c0ad859c48320700950b
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 21:53:19 +0800

    pseudocode

---
+++ batch_4/ask_user_nums_until_input_invalid_display_number_with_duplicates.py
@@ -0,0 +1,11 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate
+
+# initialize an empty list for storing entered number
+
+# ask user 10 numbers
+
+# initialize an empty list for counting duplicate numbers
+
+# process
+
+# display numbers that have duplicate
