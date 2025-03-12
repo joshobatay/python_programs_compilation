b64387366c6c93433bf33393dcec17c24de2f11e
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 18:13:20 +0800

    initialized an empty list and added input

--- batch_3/ask_user_10_num_print_if_have_no_duplicate.py
+++ batch_3/ask_user_10_num_print_if_have_no_duplicate.py
@@ -1,6 +1,12 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
+# initialize an empty list for storing entered number
+entered_numbers = []
+
 # ask user 10 numbers
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    entered_numbers.append(num)
 
 # process
 
