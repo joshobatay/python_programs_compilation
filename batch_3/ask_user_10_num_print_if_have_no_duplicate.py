c04db3e47a669d1c699dd966704abfe0c675455f
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 18:42:17 +0800

    change {i+1} to {i + 1} for better readability and added try-except for error handling

--- batch_3/ask_user_10_num_print_if_have_no_duplicate.py
+++ batch_3/ask_user_10_num_print_if_have_no_duplicate.py
@@ -1,20 +1,25 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
-# initialize an empty list for storing entered number
-entered_numbers = []
+# try-except for error handling
+try: 
+    # initialize an empty list for storing entered number
+    entered_numbers = []
 
-# ask user 10 numbers
-for i in range(10):
-    num = int(input(f"Enter number {i+1}: "))
-    entered_numbers.append(num)
+    # ask user 10 numbers
+    for i in range(10):
+        num = int(input(f"Enter number {i + 1}: "))
+        entered_numbers.append(num)
 
-# initialize an empty list for counting non-duplicate numbers
-unique_numbers = []
+    # initialize an empty list for counting non-duplicate numbers
+    unique_numbers = []
 
-# process
-for num in entered_numbers:
-    if entered_numbers.count(num) == 1:
-        unique_numbers.append(num)
+    # process
+    for num in entered_numbers:
+        if entered_numbers.count(num) == 1:
+            unique_numbers.append(num)
 
-# print numbers that dont have duplicate
-print(f"The numbers that do not have duplicates are: {unique_numbers}")
+    # print numbers that dont have duplicate
+    print(f"The numbers that do not have duplicates are: {unique_numbers}")
+
+except ValueError:
+    print("Invalid input!")
