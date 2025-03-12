8c62698f636cb473d84cc1a13e2a767b1e135fc0
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 22:33:26 +0800

    initialized an empty set, added while true input, process, and output

--- batch_4/ask_user_nums_until_invalid_input_display_highest_number.py
+++ batch_4/ask_user_nums_until_invalid_input_display_highest_number.py
@@ -1,7 +1,15 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
+# initialize an empty set for storing entered numbers
+entered_numbers = []
+
 # ask user infinite times
+while True:
+    
+    # process
+    num = int(input("Enter a number: "))
+    entered_numbers.append(num)
+    print(f"The highest number so far: {max(entered_numbers)}") # displays the highest number
 
-# process
 
-# display highest number
+
