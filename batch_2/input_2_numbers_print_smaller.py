620ed1bfeca34c1a3427478388e9dfdccf33a609
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:34:25 +0800

    added process and output

--- batch_2/input_2_numbers_print_smaller.py
+++ batch_2/input_2_numbers_print_smaller.py
@@ -5,5 +5,12 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # process
+if num1 < num2:
+    print(f"{num1} is smaller than {num2}") # output
+    
+elif num2 < num1:
+    print(f"{num2} is smaller than {num1}") # output
+else:
+    
+    print("Both numbers are equal") # output
 
-# output
