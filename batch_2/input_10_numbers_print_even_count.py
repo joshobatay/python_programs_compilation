16337d94fc5bd452f48597e149f37258c5672597
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 17:14:13 +0800

    initilialized a value and added input

--- batch_2/input_10_numbers_print_even_count.py
+++ batch_2/input_10_numbers_print_even_count.py
@@ -1,7 +1,12 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
+# initialize a value
+even_count = 0
+
 # input
-
-# process
-
+for i in range(10): # ask user 10 times
+    num = int(input(f"Enter number {i+1}: "))
+    if num % 2 == 0: # process
+        even_count+=1
+        
 # output
