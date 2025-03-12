9ea356df01e39d77626653d939b7f4c520cbd87f
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 18:39:43 +0800

    added try-except for error handling

--- batch_3/ask_user_10_nums_display_first_entry_only.py
+++ batch_3/ask_user_10_nums_display_first_entry_only.py
@@ -1,19 +1,26 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
-# initialize an empty list for storing entered number
-entered_numbers = []
+# try-except for error handling
+try:
+    
+    # initialize an empty list for storing entered number
+    entered_numbers = []
 
-# ask user 10 numbers
-for i in range(10):
-    num = int(input(f"Enter a number {i + 1}: "))
-    entered_numbers.append(num)
+    # ask user 10 numbers
+    for i in range(10):
+        num = int(input(f"Enter a number {i + 1}: "))
+        entered_numbers.append(num)
 
-# Initialize an empty list to store numbers while keeping only first entry of a duplicate
-display_numbers = []
+    # Initialize an empty list to store numbers while keeping only first entry of a duplicate
+    display_numbers = []
 
-# process
-for num in entered_numbers:
-    if num not in display_numbers:
-        display_numbers.append(num)
+    # process
+    for num in entered_numbers:
+        if num not in display_numbers:
+            display_numbers.append(num)
 
-# outputs numbers, and duplicates for once only
+    # outputs numbers, and duplicates for once only
+    print(f"{display_numbers}")
+
+except ValueError:
+    print("Invalid input!")
