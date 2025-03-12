710d5304488b0a19590c74d49850bbc02e1ae5fa
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:42:20 +0800

    added input

--- batch_2/input_2_numbers_print_difference.py
+++ batch_2/input_2_numbers_print_difference.py
@@ -1,6 +1,8 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 # input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 # process
 
