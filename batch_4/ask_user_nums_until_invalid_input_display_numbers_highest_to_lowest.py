de07fe976d5d53799895293376eafe0ec6a38b5f
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 22:43:46 +0800

    pseudocode

---
+++ batch_4/ask_user_nums_until_invalid_input_display_numbers_highest_to_lowest.py
@@ -0,0 +1,7 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+# initialize an empty list for the numbers to be stored
+
+# ask user for number infinite times
+
+# output numbers from highest to lowest
