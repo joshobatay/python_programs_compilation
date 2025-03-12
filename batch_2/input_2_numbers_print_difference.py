847a47382817a94ee61b07b91531d3a0fae083a0
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 16:45:53 +0800

    added try-except for error handling

--- batch_2/input_2_numbers_print_difference.py
+++ batch_2/input_2_numbers_print_difference.py
@@ -1,11 +1,17 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
-# input
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# try-except for error handling
+try:
+    
+    # input
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-# process
-difference = num1 - num2
+    # process
+    difference = num1 - num2
 
-# output
-print(f'The difference of {num1} and {num2} is {difference}')
+    # output
+    print(f'The difference of {num1} and {num2} is {difference}')
+
+except ValueError:
+    print("Invalid input!")
