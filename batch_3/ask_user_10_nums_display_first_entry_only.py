5afec00be6ba76d4b0cb5dbd249b5ce49d3cb27a
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 18:27:03 +0800

    pseudocode

---
+++ batch_3/ask_user_10_nums_display_first_entry_only.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# ask user 10 numbers
+
+# process
+
+# outputs numbers, and duplicates for once only
