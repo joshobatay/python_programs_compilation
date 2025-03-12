bbeee01f0640b943cd89673ab3ddb53391c54ffe
Josh Obatay <joshobatay2005@gmail.com>
2025-03-12 18:44:17 +0800

    pseudocode

--- batch_3/ask_user_nums_until_invalid_check_if_unique_or_duplicate.py
+++ batch_3/ask_user_nums_until_invalid_check_if_unique_or_duplicate.py
@@ -1,7 +1,6 @@
-# # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate
 
-# input
-
+# ask user infinite times until input is invalid
 
 # process
 
